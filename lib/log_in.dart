1d09287a3c23c9d4f85432f109f8d3b61a680b63
Rampagg3 <saugatadhikari2010@gmail.com>
2021-05-03 09:58:03 +0545

    Modularizing codes

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -13,32 +13,40 @@ class _LogInState extends State<LogIn> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xff444444),
-        body: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                child: TopText(
-                  txtFile: 'Sign In',
-                ),
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/form.png'),
+                fit: BoxFit.cover,
               )
-            ),
-            Center(
-              child: InForm(
-                childNote: Padding(
-                  padding: const EdgeInsets.all(50.0),
-                  child: Text(
-                    'Form Goes Here',
-                    style: TextStyle(
-                      color: Colors.black,
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  child: TopText(
+                    txtFile: 'Sign In',
+                  ),
+                )
+              ),
+              Center(
+                child: InForm(
+                  childNote: Padding(
+                    padding: const EdgeInsets.all(50.0),
+                    child: Text(
+                      'Form Goes Here',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Buttons(colour: Color(0xff52912E), dim: EdgeInsets.fromLTRB(0, 20, 0, 20), txt: 'Continue'),
-            Buttons(colour: Color(0xff000000), dim: EdgeInsets.fromLTRB(0, 0, 0, 0), txt: 'Back', click: (){ Navigator.pop(context); })
-          ],
+              Buttons(colour: Color(0xff52912E), dim: EdgeInsets.fromLTRB(0, 20, 0, 20), txt: 'Continue'),
+              Buttons(colour: Color(0xff000000), dim: EdgeInsets.fromLTRB(0, 0, 0, 0), txt: 'Back', click: (){ Navigator.pop(context); })
+            ],
+          ),
         ),
       ),
     );
