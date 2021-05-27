0f321b3d9f55ed82e5971764bc7bd1a5ae945a07
Rampagg3 <saugatadhikari2010@gmail.com>
2021-05-27 09:27:33 +0545

    Appointment UI update => Minor design changes v0.3

--- lib/Pages/appointments/list_page/components/list_doc.dart
+++ lib/Pages/appointments/list_page/components/list_doc.dart
@@ -46,80 +46,68 @@ class DocCard extends StatelessWidget {
         child: SizedBox(
           width: MediaQuery.of(context).size.width - 40,
           height: MediaQuery.of(context).size.height * 0.22,
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.2,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      spreadRadius: 2,
-                      blurRadius: 30,
-                      color: Color(0xffe5e5e5),
-                    )
-                  ]
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Card(
+            margin: EdgeInsets.only(bottom: 20),
+            elevation: 20,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        image: AssetImage('images/tmp.jpg'),
+                        fit: BoxFit.fill,
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  Column(
                     children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.35,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          image: DecorationImage(
-                            image: AssetImage('images/tmp.jpg'),
-                            fit: BoxFit.fill,
-                          )
-                        ),
+                      Text(
+                        text,
+                        style:
+                            TextStyle(color: Colors.black, fontFamily: 'Bebas', fontSize: MediaQuery.of(context).size.width * 0.063),
+                        textAlign: TextAlign.center,
                       ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.05,
+                      Text(
+                        speciality,
+                        style:
+                        TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
+                        textAlign: TextAlign.center,
                       ),
-                      Column(
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                      Row(
                         children: [
+                          ...List.generate(5, (index) => Icon(FeatherIcons.star, size: MediaQuery.of(context).size.width * 0.04,color: Colors.amber,)),
+                          SizedBox(width: MediaQuery.of(context).size.width * 0.02,),
                           Text(
-                            text,
-                            style:
-                                TextStyle(color: Colors.black, fontFamily: 'Bebas', fontSize: MediaQuery.of(context).size.width * 0.063),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            speciality,
-                            style:
-                            TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
-                          Row(
-                            children: [
-                              ...List.generate(5, (index) => Icon(FeatherIcons.star, size: MediaQuery.of(context).size.width * 0.04,color: Colors.amber,)),
-                              SizedBox(width: MediaQuery.of(context).size.width * 0.02,),
-                              Text(
-                                '4.5',
-                                style:
-                                TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          ),
-                          Text(
-                            '120 Reviews',
+                            '4.5',
                             style:
                             TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
                             textAlign: TextAlign.center,
                           ),
                         ],
                       ),
+                      Text(
+                        '120 Reviews',
+                        style:
+                        TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
+                        textAlign: TextAlign.center,
+                      ),
                     ],
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
