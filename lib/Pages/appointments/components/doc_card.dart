ba9fb81384ebeccd266fe71d4ddbf4563bfd9529
Rampagg3 <saugatadhikari2010@gmail.com>
2021-05-30 08:34:44 +0545

    Appointment swiper added

---
+++ lib/Pages/appointments/components/doc_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class DocCard extends StatelessWidget {
+  DocCard({this.text, this.press, this.speciality});
+
+  final String text;
+  final Function press;
+  final String speciality;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          height: MediaQuery.of(context).size.height * 0.22,
+          child: Card(
+            margin: EdgeInsets.only(bottom: 20, left: 10),
+            elevation: 20,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        image: DecorationImage(
+                          image: AssetImage('images/tmp.jpg'),
+                          fit: BoxFit.fill,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        text,
+                        style:
+                        TextStyle(color: Colors.black, fontFamily: 'Bebas', fontSize: MediaQuery.of(context).size.width * 0.063),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        speciality,
+                        style:
+                        TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                      Row(
+                        children: [
+                          ...List.generate(5, (index) => Icon(FeatherIcons.star, size: MediaQuery.of(context).size.width * 0.04,color: Colors.amber,)),
+                          SizedBox(width: MediaQuery.of(context).size.width * 0.02,),
+                          Text(
+                            '4.5',
+                            style:
+                            TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                      Text(
+                        '120 Reviews',
+                        style:
+                        TextStyle(color: Colors.grey, fontFamily: 'Muli', fontSize: MediaQuery.of(context).size.width * 0.04),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
