22c20e80fa068a5fe180e8b738669ab4442f883a
tyrafero <lehanhunter@gmail.com>
2021-06-01 01:54:59 +0545

    images added=> some more changes

--- lib/details/Components/body.dart
+++ lib/details/Components/body.dart
@@ -8,7 +8,66 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProductImages(product: product);
+    return Column(
+      children: [
+        ProductImages(product: product),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width*0.04,
+          ),
+          child: TopRoundedContainer(
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.title,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width*0.04,
+                ),
+                  child: Text(
+                    product.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TopRoundedContainer extends StatelessWidget {
+  const TopRoundedContainer({
+
+    Key key,
+    @required this.child,@required this.color,
+  }) : super(key: key);
+  final Widget child;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.width*0.02),
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.width*0.02),
+      width: double.infinity,
+      //height: MediaQuery.of(context).size.width*0.70,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        )
+
+      ),
+
+    );
   }
 }
 
